b0b77dc1d8beee05d56c9669f9864770b922522b
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-01-23 09:56:05 +0700

    36. Jenis Dependency Injection Pada Spring

--- book-catalog/src/main/java/com/sibeux/domain/Book.java
+++ book-catalog/src/main/java/com/sibeux/domain/Book.java
@@ -4,7 +4,12 @@ public class Book {
     private String title;
     private Author author;
 
-    public Book(Author author){
+    public Book(Author author) {
+        this.author = author;
+    }
+
+    public Book(String title, Author author) {
+        this.title = title;
         this.author = author;
     }
 
