ce35bb4684b8ff1843d1b1f14c61d7628f511f71
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-01-23 09:02:45 +0700

    34. Membuat Bean Pertama

--- book-catalog/src/main/java/com/sibeux/App.java
+++ book-catalog/src/main/java/com/sibeux/App.java
@@ -1,13 +1,24 @@
 package com.sibeux;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sibeux.domain.Author;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try (ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
+                "application-context.xml")) {
+            Author author = (Author) appContext.getBean("author");
+            System.out.println(author.getId());
+            System.out.println(author.getName());
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
     }
 }
